218c4e12c05cee580defbc4b4fd1d16023595fd1
Denis <fortronin364@gmail.com>
2026-09-24 18:00:43 +0300

    1-5

--- 3/src/Card.jsx
+++ 3/src/Card.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
-// BEGIN (write your solution here)
+const Card = (props) => {
+  const title = props.title ?? 'title';
+  const text = props.text ?? 'text';
 
-// END
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h4 className="card-title">{title}</h4>
+        <p className="card-text">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- 5/src/Progress.jsx
+++ 5/src/Progress.jsx
@@ -2,4 +2,20 @@ import React from 'react';
 
 // BEGIN (write your solution here)
 
+const Progress = ({ percentage }) => (
+  <div className="progress">
+    <div
+      className="progress-bar"
+      role="progressbar"
+      aria-valuenow={percentage}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      aria-label="progressbar"
+      style={{ width: `${percentage}%` }}
+    />
+  </div>
+);
+
+export default Progress;
+
 // END
